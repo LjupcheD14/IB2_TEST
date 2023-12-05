cc6cfdc4d9643bb87f45935d5d941cfd89de0eb2
Ljubomir <ljupcedavitkov14@gmail.com>
2023-12-05 01:51:46 +0100

    First version

---
+++ src/main/java/com/example/ib2/Ib2Application.java
@@ -0,0 +1,13 @@
+package com.example.ib2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ib2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ib2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/ib2/Ib2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ib2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ib2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
